347a0198c2a07a74793a4cc24f0fa367f8eaf0b2
Baytemur <brfnbytmr@gmail.com>
2021-07-18 23:45:00 +0300

    Game Release

---
+++ Assets/Scprits/ZeminSpawner.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZeminSpawner : MonoBehaviour
+{
+    public GameObject son_zemin;
+
+    void Start ()
+    {
+        for (int i = 0; i < 15; i++)
+        {
+            zemin_oluştur();
+            i++;
+        }
+    }
+
+    public void zemin_oluştur()
+    {
+        Vector3 yön;
+        //Bana bir rastgele değer oluştur bu değer 0 ya da 1 olsun
+        if(Random.Range(0,2) == 0)
+        {
+            yön = Vector3.left;
+        }
+        else
+        {
+            yön = Vector3.forward;
+        }
+        son_zemin = Instantiate(son_zemin, son_zemin.transform.position + yön, son_zemin.transform.rotation);
+    }
+}
